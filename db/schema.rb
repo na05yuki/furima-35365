7ef2b801cef93a8b0d4fe3c40ba24cc6fe819100
na05yuki <pempem.kmkm@gmail.com>
2021-07-23 16:46:17 +0900

    usersテーブルのカラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2021_07_12_023637) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
