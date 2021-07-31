80a789ce245a0ff9a19e3c816b8dd8f254728cba
na05yuki <pempem.kmkm@gmail.com>
2021-07-31 16:43:48 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
-  validates :name, :price, presence: true
+  validates :name, :explanation, :price, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
