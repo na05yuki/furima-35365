dce89c634da38c636644e6551a54687a73457513
na05yuki <pempem.kmkm@gmail.com>
2021-07-31 16:48:06 +0900

    商品出品画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
-  
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id)
+  end
+
 end
