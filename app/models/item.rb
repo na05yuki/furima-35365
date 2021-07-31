e89bbeb5f13a24eef2419b99b47ace2f49fbdb33
na05yuki <pempem.kmkm@gmail.com>
2021-07-31 14:19:16 +0900

    category_idのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :name, :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
+
 end
