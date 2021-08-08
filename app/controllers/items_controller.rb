1bd4ac1d835ea301421fe0de6aef809bb1826d62
na05yuki <pempem.kmkm@gmail.com>
2021-08-08 14:58:27 +0900

    商品編集機能実装の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
