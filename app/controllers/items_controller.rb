533d1d457df32dbd3587238ba02b9c6c157edbbb
na05yuki <pempem.kmkm@gmail.com>
2021-08-08 15:36:03 +0900

    商品情報詳細機能の修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
-    @item = Item.all
     @items = Item.order('created_at DESC')
   end
 
@@ -29,7 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to item_path
+      redirect_to item_path(@item)
     else
       render :edit
     end
