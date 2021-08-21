aa3e3217ff6b77ce916cdadacdca5101cdf79ed8
na05yuki <pempem.kmkm@gmail.com>
2021-08-21 16:53:05 +0900

    購入済み商品の表示変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :sold_out_item
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -17,10 +21,19 @@ class OrdersController < ApplicationController
     end
   end
 
+
   private
 
   def order_params
     params.require(:order_address).permit(:zip_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_management_id).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def sold_out_item
+    redirect_to root_path if @item.purchase_management.present?
   end
 
   def pay_item
